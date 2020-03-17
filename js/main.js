adeb94886e695fdee3481bc72e895ad4cd91dff6
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-03-17 11:56:45 -0400

    Add missing semicolon

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var below = ['Potato', 'Turnip', 'Carrot', 'Onion'];
 
 var $h2b = $('<h2>Below ground veggies!</h2>');
 var $h2a = $('<h2>Above ground veggies!</h2>');
-var $ul = $('<ul>')
+var $ul = $('<ul>');
 
 $('body').prepend($h2a);
 $('body').prepend($ul);
